cea3d78c5a0000e9bba3e2adc85ac0a312ff02cc
ShemLeon <shemiakinleonid@gmail.com>
2026-01-17 22:51:17 +0200

    splashscreen start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ android {
 
 
 dependencies {
+
+    implementation(libs.androidx.core.splashscreen.v100)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
