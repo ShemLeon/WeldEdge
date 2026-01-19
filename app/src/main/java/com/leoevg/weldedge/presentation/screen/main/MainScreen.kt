80ac4a55ba13771d2635966802dc6f4d4d7dc441
ShemLeon <shemiakinleonid@gmail.com>
2026-01-19 18:10:26 +0200

    preview

--- app/src/main/java/com/leoevg/weldedge/presentation/screen/main/MainScreen.kt
+++ app/src/main/java/com/leoevg/weldedge/presentation/screen/main/MainScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -466,6 +467,21 @@ fun DocumentPreview(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreenContent(
+        state = MainScreenState(
+            params = WeldingParams(
+                metalType = "нержавейка",
+                thickness = "3.0",
+                jointType = "стык"
+            )
+        ),
+        onEvent = {}
+    )
 }
 
 @Composable
